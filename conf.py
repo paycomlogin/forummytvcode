383ec35bd9566e66f995c2b3df7e5b1c15ff6934
paycomlogin <109283393+paycomlogin@users.noreply.github.com>
2025-07-09 12:25:27 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'MyTV Code'
 
 
 # The full version, including alpha/beta/rc tags
